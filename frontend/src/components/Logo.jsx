8ff1730ea0c4ef85f2f2772c6dadf137636bf157
brayney <bryneborinaga15@gmail.com>
2026-07-13 00:50:11 +0800

    guessing

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function Logo({ className = '', size = 84 }) {
+export default function Logo({ className = '', size = 96 }) {
   return (
     <div className={className.trim()} style={{ width: size, height: size }}>
       <img
         src="/logo.png"
         alt="nearbi logo"
-        className="block h-full w-full object-cover"
+        className="block h-full w-full object-contain"
         style={{ width: '100%', height: '100%' }}
       />
     </div>
